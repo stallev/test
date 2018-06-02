8127cb0ee4e7768c3b1f5797312f39538df852b4
alex <stwork87@gmail.com>
2018-06-02 18:24:23 +0300

    added smart-grid

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,45 @@ var run = require('run-sequence');
 var del = require('del');
 var rename = require('gulp-rename');
 var ghPages = require('gulp-gh-pages');
+var smartGrid = require('smart-grid');
+
+gulp.task('smartGR', function(){
+  var settingsSmartGrid = {
+    outputStyle: 'scss', /* less || scss || sass || styl */
+    columns: 12, /* number of grid columns */
+    offset: '30px', /* gutter width px || % */
+    mobileFirst: false, /* mobileFirst ? 'min-width' : 'max-width' */
+    container: {
+        maxWidth: '1200px', /* max-width оn very large screen */
+        fields: '30px' /* side fields */
+    },
+    breakPoints: {
+        lg: {
+            width: '1100px', /* -> @media (max-width: 1100px) */
+        },
+        md: {
+            width: '960px'
+        },
+        sm: {
+            width: '780px',
+            fields: '15px' /* set fields only if you want to change container.fields */
+        },
+        xs: {
+            width: '560px'
+        }
+        /* 
+        We can create any quantity of break points.
+ 
+        some_name: {
+            width: 'Npx',
+            fields: 'N(px|%|rem)',
+            offset: 'N(px|%|rem)'
+        }
+        */
+    }
+};
+return smartGrid('sass', settingsSmartGrid);
+})
 
 gulp.task('style', function() {
   gulp.src('sass/style.scss')
@@ -95,7 +134,7 @@ gulp.task('images', function() {
 });
 
 gulp.task('build', function(fn){
-  run('clean', 'copy', 'copyBootstrapJS', 'images', 'style', fn);
+  run('clean', 'copy', 'copyBootstrapJS', 'images', 'smartGR', 'style', fn);
 });
 
 gulp.task('server', function(){
